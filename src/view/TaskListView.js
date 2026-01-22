0b0ab5391f2398f4342737f923c638a101043488
maximemlly <maxime.maillary@icloud.com>
2026-01-22 14:46:46 +0100

    Add task toggle functionality and edit button callback in TaskListView

--- src/view/TaskListView.js
+++ src/view/TaskListView.js
@@ -43,17 +43,52 @@ export class TaskListView {
 
   #createEmptyElement(task) {
     const taskElement = document.createElement("li");
+    taskElement.classList.add("task");
 
+    // ? Création de l'élément toggle
     const taskToggleElement = DocumentFragment.createElement("input");
     taskToggleElement.setAttribute("type", "checkbox");
+    taskToggleElement.classList.add("task-toggle");
+    taskToggleElement.addEventListener("change", () => {
+      if (typeof this.#toggleCallback === "function") {
+        this.#toggleCallback(task.id);
+      }
+    });
 
+    // ? Création de l'élément de titre
     const taskTitleElement = document.createElement("p");
     taskTitleElement.classList.add("add-title");
     taskTitleElement.textContent = task.title;
     taskTitleElement.dataset.taskId = task.id;
 
+    const taskActionsElement = docimenet.createElement("div");
+    taskActionsElement.classList.add("task-actions");
+
+    // ? Bouton "Éditer"
     const taskEditElement = document.createElement("button");
+    taskEditElement.textContent = "Éditer";
+    taskEditElement.classList.add("button", "button--primary");
+    taskEditElement.setAttribute("type", "button");
+    taskEditElement.addEventListener("click", () => {
+      if (typeof this.#editCallback === "function") {
+        this.#editCallback(task.id);
+      }
+    });
+
+    // ? Boutton "Supprimer"
     const taskDeleteElement = document.createElement("button");
+    taskDeleteElement.textContent = "Supprimer";
+    taskDeleteElement.classList.add("button", "button--danger");
+    taskDeleteElement.setAttribute("type", "button");
+    taskDeleteElement.addEventListener("click", () => {
+      if (typeof this.#editCallback === "function") {
+        this.#editCallback(task.id, task.title);
+      }
+    });
+
+    taskActionsElement.append(taskEditElement, taskDeleteElement);
+
+    taskElement.append(taskTitleElement, taskToggleElement);
 
     return taskElement;
   }
